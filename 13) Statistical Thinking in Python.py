7ffe54d040f11479dbb72d515e5e8a37dbce7031
emilegill743 <58830015+emilegill743@users.noreply.github.com>
2020-02-20 17:00:41 +0000

    Added Thinking probabilistically - Discrete variables to Statistical Thinking in Python

--- 13) Statistical Thinking in Python.py
+++ 13) Statistical Thinking in Python.py
@@ -143,7 +143,121 @@
 '''Thinking probabilistically - Discrete variables'''
 
     # Random number generators and hacker statistics
-     
+
+        # Hacker statistics - Uses simulated repeat measurements to compute probabilities
+            
+            # Determine how to simulate data
+            # Simulate many times
+            # Compute fraction of trials with outcome of interest
+
+
+        # np.random module - suite of functions based on random number generation
+
+        # Simulating a coin flip
+        np.random.random() # draws random number [0,1], equal prob
+        # Heads < 0.5
+        # Tails >= 0.5
+        
+        # Bernouli trial - 
+            # experiment with two options, success (True) and failure (False)
+
+            def perform_bernoulli_trials(n, p):
+
+                """Perform n Bernoulli trials with success probability p
+                and return number of successes."""
+
+                # Initialize number of successes: n_success
+                n_success = 0
+
+                # Perform trials
+                for i in range(n):
+                    # Choose random number between zero and one: random_number
+                    random_number = np.random.random()
+
+                    # If less than p, it's a success so add one to n_success
+                    if random_number < p:
+                        n_success += 1
+
+                return n_success
+
+        # Random number seed - integer fed into random number generating algorithm
+        # Manually seed random number generator for reproducability
+        np.random.seed()
+
+        # Simulating 4 coin flips
+            import numpy as np
+            np.random.seed(42)
+            random_numbers = np.random.random(size=4)
+
+            heads = random_numbers < 0.5
+            np.sum(heads)
+
+        # Calculating the probability of 4 heads
+
+            n_all_heads = 0 # Initialise no. trials with 4 heads
+
+            # Simulate 10000 trials of 4 flips
+            for _ in range(10000):
+                heads = np.random.random(size=4) < 0.5
+                n_heads = np.sum(heads)
+                if n_heads == 4:
+                    n_all_heads += 1
+
+        # Predicting probability of defaults on a loan
+
+            # 100 loans, p = 0.05 prob of default
+            # Predicting probability of number of defaults from batch of 100 loans
+            
+            # Seed random number generator
+            np.random.seed(42)
+
+            # Initialize the number of defaults: n_defaults
+            n_defaults = np.empty(1000)
+
+            # Compute the number of defaults
+            for i in range(1000):
+                n_defaults[i] = perform_bernoulli_trials(100, 0.05)
+
+
+            # Plot the histogram with default number of bins; label your axes
+            _ = plt.hist(n_defaults, normed=True)
+            _ = plt.xlabel('number of defaults out of 100 loans')
+            _ = plt.ylabel('probability')
+
+            # Show the plot
+            plt.show()
+
+        # Probability of bank losing money
+            
+            # Compute ECDF: x, y
+            x, y = ecdf(n_defaults)
+
+            # Plot the ECDF with labeled axes
+            plt.plot(x,y, marker='.', linestyle='none')
+            plt.xlabel('no. deafaults')
+            plt.ylabel('ecdf')
+            plt.show()
+
+            # Compute the number of 100-loan simulations with 10 or more defaults: n_lose_money
+            n_lose_money = np.sum(n_defaults >= 10)
+
+            # Compute and print probability of losing money
+            print('Probability of losing money =', n_lose_money / len(n_defaults))
+
+    # Probability distributions
+
+        # Probability mass function - set of probabilities of discrete outcomes
+        #  Probability distribution - mathematical description of outcomes
+
+        # Discrete Uniform Distribution
+            # The outcome of rolling a single fair die is discrete, uniformly distributed.
+
+        # Binomial distribution
+            # The number r of successes in n Bernoulli trials,
+            # with probability p of success is Binomially distributed.
+
+            np.binomial(n, p, size=size)
+
 '''Introduction to Hypothesis Testing'''
 
     # Hypothesis Testing - Assessment of how reasonable the observed data are, assuming a hypothesis is true
